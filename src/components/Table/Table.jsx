3f519461836de9992878eaf81649be372859f789
Lucas Leite <lucas.leite.ext@nokia.com>
2021-06-29 14:49:50 +0100

    table adjustments; added refresh tasks button

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -4,19 +4,26 @@ import './Table.css';
 
 const Table = (props) => {
 
-  const { list } = props;
+  const { list, refreshCallback } = props;
 
   const getRows = (tasks) => {
     if (!(tasks instanceof Array) || !tasks.length) {
-      return [];
+      return (
+        <tr>
+          <td
+            className="empty"
+            colSpan="3"
+          >No tasks available.</td>
+        </tr>
+      );
     }
     return tasks.map(task => {
       return (
         <tr key={ task.id }>
           <th scope="row">{ task.id }</th>
           <td>{ task.text }</td>
-          <td className={ task.completed ? 'done' : 'todo' } >
-            <i className={ task.completed ? 'bi bi-check-square' : 'bi bi-x-square' } ></i>
+          <td className={ task.completed ? 'done' : 'todo' }>
+            <i className={ task.completed ? 'bi bi-check-square' : 'bi bi-x-square' } />
           </td>
         </tr>
       );
@@ -27,7 +34,15 @@ const Table = (props) => {
     <table className="table">
       <thead>
         <tr>
-          <th scope="col">#</th>
+          <th scope="col">
+            <button 
+              className="refresh"
+              onClick={refreshCallback}
+              title="Refresh Tasks"
+            >
+              <i className="bi bi-arrow-counterclockwise" />
+            </button>
+          </th>
           <th scope="col">Description</th>
           <th scope="col">Status</th>
         </tr>
--- src/views/Tasks/Tasks.jsx
+++ src/views/Tasks/Tasks.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from "react-router-dom";
 import axios from 'axios';
 
 import Table from '../../components/Table/Table';
@@ -6,20 +7,31 @@ import Table from '../../components/Table/Table';
 const Tasks = () => {
 
   const [ list, setList ] = useState([]);
+  const currLocation = useLocation().pathname;
 
-  useEffect(() => {
+  const refreshTasks = () => {
     axios.get('http://localhost:8080/api/tasks')
-      .then(tasks => setList(tasks.data))
+      .then(tasks => {
+        if (currLocation === '/tasks') {
+          setList(tasks.data);
+        }
+      })
       .catch(e => {
-        setList([]);
+        if (currLocation === '/tasks') {
+          setList([]);
+        }
         console.log(e);
       });
+  };
+
+  useEffect(() => {
+    refreshTasks();
   }, []);
 
   return (
     <div className="tasks">
       <h1 className="title">Tasks</h1>
-      <Table list={list} />
+      <Table list={list} refreshCallback={refreshTasks}/>
     </div>
   );
 }
